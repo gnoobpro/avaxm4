3220c767c2414c9b29e7b101f295d02b2bd36bc7
gnoobpro <120583216+gnoobpro@users.noreply.github.com>
2023-05-16 23:35:39 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,13 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts@4.8.3/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.8.3/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.8.3/access/Ownable.sol";
 
-contract DegenToken is ERC20, Ownable {
+contract Degen is ERC20, ERC20Burnable, Ownable {
     constructor() ERC20("Degen", "DGN") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
+     function transfer(address to,  uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function redeemTokens(uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+
+        _burn(msg.sender, amount);
+}
 }
